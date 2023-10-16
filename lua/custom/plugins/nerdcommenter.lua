70b0cb268d53f60018105e64d653b88db3a3103c
pastelInc <keyaki765@gmail.com>
2023-10-17 00:26:20 +0900

    Add nerdcommenter plugin support

---
+++ lua/custom/plugins/nerdcommenter.lua
@@ -0,0 +1,13 @@
+vim.cmd([[
+  let g:NERDSpaceDelims = 1
+  let g:NERDDefaultAlign='left'
+]])
+
+return {
+  "preservim/nerdcommenter",
+  version = "*",
+  dependencies = {
+  },
+  config = function ()
+  end,
+}
